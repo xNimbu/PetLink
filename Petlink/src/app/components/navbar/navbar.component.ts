32bb342ddeb137c3e61d0aceb94eaf5434f616b7
xNimbu <lui.illanes@duocuc.cl>
2025-05-15 03:32:35 -0400

    Actualizar componente de navegación: agregar funcionalidad de navegación a las imágenes y enlaces de perfil

--- Petlink/src/app/components/navbar/navbar.component.ts
+++ Petlink/src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, Inject } from '@angular/core';
+import { Route, Router, RouterModule } from '@angular/router';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  constructor(private route: Router) { }
+  openHome() {
+    this.route.navigate(['/home']);
+  }
 
+  openProfile() {
+    this.route.navigate(['/profile']);
+  }
 }
