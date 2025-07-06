2623e54ed02b1a0c03cdc842eecc5fdb17dc800a
xNimbu <lui.illanes@duocuc.cl>
2025-07-06 02:05:23 -0400

    refactor: clean unused server logic from config

--- Petlink/src/app/app.config.server.ts
+++ Petlink/src/app/app.config.server.ts
@@ -2,11 +2,14 @@ import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideAuth, Auth } from '@angular/fire/auth';
 
 const serverConfig: ApplicationConfig = {
   providers: [
     provideServerRendering(),
     provideClientHydration(),
+    // Provide a dummy Auth instance so injection doesn't fail
+    provideAuth(() => null as unknown as Auth),
   ]
 };
 
