c6fdb4761a15c61a0c3cbe6d22a2a06ca703c191
xNimbu <lui.illanes@duocuc.cl>
2025-07-06 05:38:51 -0400

    Actualiza la configuración de entorno para usar variables de entorno en producción y agrega la inclusión de archivos de definición de entorno

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,17 +1,16 @@
-// src/environments/environment.prod.ts
 export const environment = {
   production: true,
+  backendUrl: import.meta.env['NG_APP_API_URL'],
+  wsUrl: import.meta.env['NG_APP_WS_URL'],
   firebaseConfig: {
-    apiKey:            '',
-    authDomain:        '',
-    databaseURL:       '',
-    projectId:         '',
-    storageBucket:     '',
-    messagingSenderId: '',
-    appId:             '',
-    measurementId:     ''
+    apiKey: import.meta.env['NG_APP_FIREBASE_API_KEY'],
+    authDomain: import.meta.env['NG_APP_FIREBASE_AUTH_DOMAIN'],
+    databaseURL: import.meta.env['NG_APP_FIREBASE_DATABASE_URL'],
+    projectId: import.meta.env['NG_APP_FIREBASE_PROJECT_ID'],
+    storageBucket: import.meta.env['NG_APP_FIREBASE_STORAGE_BUCKET'],
+    messagingSenderId: import.meta.env['NG_APP_FIREBASE_MESSAGING_SENDER_ID'],
+    appId: import.meta.env['NG_APP_FIREBASE_APP_ID'],
+    measurementId: import.meta.env['NG_APP_FIREBASE_MEASUREMENT_ID']
   },
-  login: false,
-  backendUrl: 'https://proyectotitulo.onrender.com/api',
-  wsUrl: 'wss://proyectotitulo.onrender.com/ws/chat/'
+  login: import.meta.env['NG_APP_LOGIN'] === 'true'
 };
