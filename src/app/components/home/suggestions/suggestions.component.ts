32217aaf9d9c461fe36ae10fc4fda36ac9751810
xNimbu <lui.illanes@duocuc.cl>
2025-07-09 00:02:39 -0400

    Filter logged-in user from suggestions

--- src/app/components/home/suggestions/suggestions.component.ts
+++ src/app/components/home/suggestions/suggestions.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { FriendService } from '../../../services/friends/friend.service';
+import { AuthService } from '../../../services/auth/auth.service';
 import { environment } from '../../../../environments/environment';
 
 interface UserSuggestion {
@@ -23,6 +24,7 @@ export class SuggestionsComponent implements OnInit {
 
   private http = inject(HttpClient);
   private friendService = inject(FriendService);
+  private authService = inject(AuthService);
 
   ngOnInit(): void {
     // Obtener lista de perfiles y filtrar los que ya son amigos
@@ -32,7 +34,7 @@ export class SuggestionsComponent implements OnInit {
         this.http.get<UserSuggestion[]>(`${environment.backendUrl}/profile_list/`)
           .subscribe(list => {
             this.suggestions = list
-              .filter(u => !friendIds.has(u.uid))
+              .filter(u => !friendIds.has(u.uid) && u.uid !== this.authService.uid)
               .slice(0, 5)
               .map(u => ({ ...u, requestSent: false }));
           });
@@ -41,7 +43,10 @@ export class SuggestionsComponent implements OnInit {
         // Si falla la carga de amigos, aun así intentar sugerencias
         this.http.get<UserSuggestion[]>(`${environment.backendUrl}/profile_list/`)
           .subscribe(list => {
-            this.suggestions = list.slice(0, 5).map(u => ({ ...u, requestSent: false }));
+            this.suggestions = list
+              .filter(u => u.uid !== this.authService.uid)
+              .slice(0, 5)
+              .map(u => ({ ...u, requestSent: false }));
           });
       }
     });
