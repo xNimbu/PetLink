79afe543fe4817c847d0db440d84fae76b9fd0a3
xNimbu <lui.illanes@duocuc.cl>
2025-07-08 23:55:13 -0400

    Add profile suggestions card on home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,11 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { AddPostComponent } from "./add-post/add-post.component";
 import { PostsComponent } from "./posts/posts.component";
 import { ChatsComponent } from "./chats/chats.component";
+import { SuggestionsComponent } from './suggestions/suggestions.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, FormsModule, AddPostComponent, PostsComponent, ChatsComponent],
+  imports: [CommonModule, FormsModule, AddPostComponent, SuggestionsComponent, PostsComponent, ChatsComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
